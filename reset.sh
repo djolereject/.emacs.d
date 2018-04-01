c2ae5a9b3eb3e5390dd61bae1692205116d0b2c4
Djordje Knezevic <djolereject@gmail.com>
2018-04-01 17:02:57 +0200

    link readme to settings

--- reset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find . -maxdepth 1 -not \( -name '.' -or -name 'reset.sh' -or -name '.git*' -or -name 'README*' -or -name 'init.el' -or -name 'settings.org' \) -exec rm -r {} +
-echo $PWD " is set to initial state "
