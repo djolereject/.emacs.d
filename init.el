ad327e68a7424883f23e07668d662d3487094f2e
Djordje Knezevic <djolereject@gmail.com>
2020-05-21 15:55:07 +0200

    Combine functions for opening and compiling README.

--- init.el
+++ init.el
@@ -11,16 +11,19 @@
 (setq config-dir (expand-file-name "config/" user-emacs-directory))
 (setq data-dir (expand-file-name "data/" user-emacs-directory))
 
-;; Create directories if they don't exist
+;; Create those directories if they don't exist
 ;;
 (unless (file-exists-p data-dir) (make-directory data-dir))
 (unless (file-exists-p config-dir) (make-directory config-dir))
+
+;; Set global variables for README.org file and config/settings.el, one is original source of code and the other one is what babel tangles
+;;
+(setq original-source (expand-file-name "README.org" user-emacs-directory))
+(setq compiled-source (expand-file-name "settings.el" config-dir))
 
 ;; `org-babel-load-file` doesn't work anymore when destination file is different that original with .el extension, something changed with Emasc 27 or org.
 ;; `org-babel-tangle-file` and `load-file` are replacing that function.
 ;;
 (require 'org)
-(let ((source (expand-file-name "README.org" user-emacs-directory))
-      (target (expand-file-name "settings.el" config-dir)))
-  (org-babel-tangle-file source target)
-  (load-file target))
+(org-babel-tangle-file original-source compiled-source)
+(load-file compiled-source)
